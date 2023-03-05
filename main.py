8fce4550383f1f217f9403981b74e88304ad6df1
adhfmz7 <ahmadaldasouqi559@gmail.com>
2023-03-05 11:50:55 -0800

    fix offset

--- main.py
+++ main.py
@@ -1,51 +1,74 @@
 #!/opt/anaconda3/envs/game/bin/python3
+
 import parse
 import numpy as np
 import pygame
-from matrices import proj, rot
 import sys
+import time
+from math import cos, sin
+
+SCREEN_WIDTH = 1920 
+SCREEN_HEIGHT = 1080 
+SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+OFFSET = np.array([SCREEN_WIDTH/2, SCREEN_HEIGHT/2])
+PM = np.array([[1, 0, 0],
+               [0, 1, 0]])
 
 def drawCube(cube, angle):
 
   for i in range(4):
-    pygame.draw.line(screen, WHITE, proj(rot(cube[i], angle)) + (SCREEN_WIDTH / 2), 
-                                    proj(rot(cube[(i + 1) % 4] + (SCREEN_WIDTH / 2), angle)))
+    pygame.draw.line(screen, WHITE, proj(rot(cube[i], angle)), 
+                                    proj(rot(cube[(i + 1) % 4], angle)))
 
 
-    pygame.draw.line(screen, WHITE, proj(rot(cube[i + 4], angle)) + (SCREEN_WIDTH / 2),
-                                    proj(rot(cube[((i + 1) % 4) + 4] + (SCREEN_WIDTH / 2), angle)))
+    pygame.draw.line(screen, WHITE, proj(rot(cube[i + 4], angle)),
+                                    proj(rot(cube[((i + 1) % 4) + 4], angle)))
 
 
-    pygame.draw.line(screen, WHITE, proj(rot(cube[i], angle)) + (SCREEN_WIDTH / 2), 
-                                    proj(rot(cube[i + 4] + (SCREEN_WIDTH / 2), angle)))
+    pygame.draw.line(screen, WHITE, proj(rot(cube[i], angle)), 
+                                    proj(rot(cube[i + 4], angle)))
 
 
-pygame.init()
-pygame.display.set_caption('py3D')
+def rot(A, angle):
+  rotX = np.array([[1, 0, 0],
+                   [0, sin(angle), -cos(angle)],
+                   [0, sin(angle), cos(angle)]])
 
-SCREEN_WIDTH = 900
-SCREEN_HEIGHT = 600
-SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
+  rotY = np.array([[cos(angle), 0, sin(angle)],
+                   [0, 1, 0],
+                   [-sin(angle), 0, cos(angle)]])
 
-screen = pygame.display.set_mode(SIZE)
+  rotZ = np.array([[cos(angle), -sin(angle), 0],
+                   [sin(angle), cos(angle), 0],
+                   [0, 0, 1]])
 
-
-cube = parse.parseOBJ("cube.obj")
-
-angle = 0
-while 1:
-
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT: sys.exit()
-
-  screen.fill(BLACK)
-  drawCube(cube, angle) 
-  pygame.display.flip()
-  angle += 0.01
-  if angle > 360:
-    angle = 0
+  return rotX @ rotY @ A
 
 
+def proj(A):
+  return (PM @ A) + OFFSET
 
+if __name__ == "__main__":
+  pygame.init()
+  pygame.display.set_caption('py3D')
+
+  screen = pygame.display.set_mode(SIZE)
+
+  cube = parse.parseOBJ("cube.obj")
+
+  angle = 0
+  while 1:
+
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT: sys.exit()
+
+    screen.fill(BLACK)
+    drawCube(cube, angle) 
+    pygame.display.flip()
+    angle += 0.01
+    if angle > 360:
+      angle = 0
+    time.sleep(0.01)
+   
--- parse.py
+++ parse.py
@@ -12,4 +12,28 @@ def parseOBJ(filename):
     if l[0] == 'v':
       vectors.append(np.array([l[1], l[2], l[3]], dtype=float))
 
+
   return vectors
+
+#class vec3:
+#
+#  def __init__(self, x=0, y=0, z=0)
+#    self.elem = [x, y, z]
+#
+#  def __add__(self, v2):
+#   
+#  def __get__(self, index):
+#    if index not in range(0, 3):
+#      print("index not found")
+#      return False
+#    return self.elem[index]
+#
+#
+#class Cube:
+#
+#  def __init__(self):
+
+class Mesh:
+  pass    
+
+
